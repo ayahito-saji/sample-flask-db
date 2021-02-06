974a4938b24f51b11f561c1b925ddd8a11d55944
Saji Ayahito <ayahito_saji@outlook.jp>
2021-02-07 01:02:02 +0900

    flaskの導入

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>index.html</h1>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
